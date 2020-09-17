727a83049cd9718503c6964ad1c13d1f8fe1573d
qfc5036 <qfc5036@psu.edu>
2020-09-17 14:43:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+
+# Author: Cui Qiaoxu qfc5036@psu.edu
+# Collaborator: John O'Toole Jbo5232@psu.edu
+# Collaborator: Ching-Ju Chen cxc6001@psu.edu
+# Section: 8
+# Breakout: 11
+def sum_n(n):
+  if n==0:
+    return n
+  else:
+    return n+sum_n(n-1)
+
+def print_n(s,n):
+  if n==0:
+    return 
+  elif n==1:
+    return print(s)
+  else:
+    return print_n(s,n-1)
+
+def run():
+  n = input ("Enter an int: ")
+  n = int(n)
+  n1 = sum_n(n)
+  print(f"sum is {n1}.")
+
+  s = input("Enter a string: ")
+  s = str(s)
+  s = print_n(s,n)
+
+if __name__=="__main__":
+    run()
